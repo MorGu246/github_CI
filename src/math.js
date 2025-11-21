60dfa1607608b2340bda9b044fb41a17db3b38a8
Mor Guetta <morgueta36@gmail.com>
2025-11-21 10:20:58 +0200

    test

---
+++ src/math.js
@@ -0,0 +1,15 @@
+function sum(a,b){
+    return a+b;
+}
+
+function sub(a,b){
+    return a-b;
+}
+
+function mod(a){
+    return a%2==0;
+}
+
+module.exports={
+    sum,sub,mod
+}
---
+++ src/tests/math.test.js
@@ -0,0 +1,4 @@
+const {sum,sub,mod}=require("../math")
+test("sum 2+3=5",()=> expect(sum(2,3)).toBe(5));
+test("sub 3-2=1",()=> expect(sub(3,2)).toBe(2));
+test("mod 6%2==0",()=> expect(mod(6)).toBe(true));
