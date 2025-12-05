af0036fb5319bfd98cc55ca54452a67559a272dd
Mor Guetta <morgueta36@gmail.com>
2025-12-05 09:57:51 +0200

    with Dockerfile

---
+++ src/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const port = 8080 ;//process.env.pory || 3671;
+const app = express();
+
+app.get("/", (req,res) => {
+    res.send("welcome my site")
+})
+app.listen(port, () => {            //server starts listening for any attempts from a client to connect at port: {port}
+    console.log(`The server is running on port${ port}....`);
+});
