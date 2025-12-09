b5fcf4eaf409269488676c2fdbc62f022b23581a
Mor Guetta <morgueta36@gmail.com>
2025-12-09 18:11:06 +0200

    with_Dockerfile_with_changes8

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const port = 8080 ;//process.env.pory || 3671;
 const app = express();
 
 app.get("/", (req,res) => {
-    res.send("welcome my site ")
+    res.send("welcome my site...")
 })
 app.listen(port, () => {            //server starts listening for any attempts from a client to connect at port: {port}
     console.log(`The server is running on port${ port}....`);
